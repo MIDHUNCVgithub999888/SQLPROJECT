a574dc2baffc7ffb11e6f5028c21ecefb3df765d
MIDHUNCVgithub999888 <122215889+MIDHUNCVgithub999888@users.noreply.github.com>
2023-01-17 17:36:50 +0530

    Update urls.py

--- NewProject/NewProject/urls.py
+++ NewProject/NewProject/urls.py
@@ -18,7 +18,6 @@ from django.urls import path,include
 from  django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import static,staticfiles_urlpatterns
-from django.conf.urls import include
 from django.urls import re_path
 
 urlpatterns = [
